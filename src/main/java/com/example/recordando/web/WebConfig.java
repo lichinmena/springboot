f43cf1cabcf001257e0a6120ee915a8d32983bd6
Luis Mena <luis_mena@outlook.com>
2020-11-07 00:12:00 -0600

    I18n, Genial, Gracias a Dios

--- src/main/java/com/example/recordando/web/WebConfig.java
+++ src/main/java/com/example/recordando/web/WebConfig.java
@@ -5,11 +5,43 @@
  */
 package com.example.recordando.web;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 /**
  *
  * @author luis
  */
-public class WebConfig 
+@Configuration
+public class WebConfig implements WebMvcConfigurer
 {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
+    }
     
-}
+    
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro)
+    {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+    
+} 
