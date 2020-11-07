45af46973152ec51857275574c37ed77c6d4c04a
Luis Mena <luis_mena@outlook.com>
2020-11-07 00:28:55 -0600

    Spring security parte 1

---
+++ src/main/java/com/example/recordando/web/SecurityConfig.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.recordando.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ *
+ * @author luis
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("{noop}123").roles("ADMIN","USER")
+                .and()
+                .withUser("user").password("{noop}123").roles("USER");
+                
+                
+    }
+}
